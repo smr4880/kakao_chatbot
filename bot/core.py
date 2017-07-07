5d78430ecb96c6824a6017890169a8d147291597
miran <smr4880@gmail.com>
2017-07-07 11:04:29 +0900

    test

--- bot/core.py
+++ bot/core.py
@@ -16,7 +16,8 @@ def run(user_key=None, sentence=None):
         bm.img_url = 'https://cdn.bulbagarden.net/upload/0/0d/025Pikachu.png'
         bm.response.append('이미지 전달')
     else:
-        bm.get_morph()
+        #bm.get_morph()
+        bm.response.append('유저키는 {0}, 문장은 {1}'.format(user_key, bm.sentence))
 
     res = bm.return_result('배우고 있습니다.')
     return res
