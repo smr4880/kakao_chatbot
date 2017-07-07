e66b5640a1630752c3bdcdab7af67b4ef701ea8c
miran <smr4880@gmail.com>
2017-07-07 10:57:43 +0900

    test

--- kakao_chatbot.py
+++ kakao_chatbot.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import request
-from bot.core import run
 import json
 
 app = Flask(__name__)
@@ -20,40 +19,8 @@ def keyboard():
 def post_message():
     user_message = request.get_json()
     user_content = user_message['content']
-    user_key = user_message['user_key']
-    print('user_content: %s' %user_content)
-    
-    response = run(user_key, user_content)
-    image_file = response.get('img')
-
-    reply_message = response.get('text', ' ')
-    reply_type = response.get('type', 'home') #type이 없을때 default = home
-
-    response_message = {'text': reply_message}
-
-    if reply_type == 'home':
-        button_label_list = [
-            '버튼 1',
-            '버튼 2',
-            '버튼 3'
-        ]
-        return json.dumps({
-            "message": response_message,
-            "keyboard": {"type": "buttons", "buttons": button_label_list}}
-        , ensure_ascii=False)
-    elif reply_type == 'img':
-        response_message['photo'] = {
-            'url': image_file,
-            'width': 640,
-            'height': 480
-        }
-
-    print('response from rosebot: %s' % response)
-    print('final response: %s' % response_message)
-
-    return json.dumps({'message': response_message}, ensure_ascii=False)
+    print user_content
+    return json.dumps({'message':{'text' : user_content}}, ensure_ascii=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
